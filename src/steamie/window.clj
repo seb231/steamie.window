518197ff4c33a688d393da0ee08f1a47447d25c6
seb231 <s.e.bailey@qmul.ac.uk>
2016-06-17 17:20:05 +0100

    Added steamweb

--- src/steamie/window.clj
+++ src/steamie/window.clj
@@ -1,25 +1,36 @@
 (ns steamie.window
+  (:require [steamweb.core :as steam])
   (:gen-class))
 
-(def seb "https://steamcommunity.com/profiles/76561198076420895/")
-
-(def acron {:name "https://steamcommunity.com/id/acron"
-            :number "https://steamcommunity.com/profiles/76561197960375033/"})
-
-(def seventeen-string-starter "7656119")
+(def steam-string-starter "7656119")
 
 (def steam-URL "https://steamcommunity.com/profiles/")
 
-(defn rand-5-str-number [] ;;;; WIP ;;;;
-  (+ (rand-int 10001) 99999))
+(defn rand-4-digit-number [] ;;;; WIP ;;;;
+  (+ (rand-int 1001) 9999))
 
 (defn generate-steam-id-query [starter]
-  (str starter (rand-5-str-number) (rand-5-str-number)))
+  (str starter (rand-4-digit-number) (rand-4-digit-number)))
 
 (defn generate-steam-URL []
-  (str steam-URL (generate-steam-id-query seventeen-string-starter) "/"))
+  (str steam-URL (generate-steam-id-query steam-string-starter) "/"))
 
 (defn -main
   "I don't do a whole lot ... yet."
   [& args]
   (println "Hello, World!"))
+
+(comment 
+  
+  (def acrons-friends (steam/friend-list k (:id acron)))
+  
+  (def acrons-friends-ids (map :steamid (get-in acrons-friends [:friendslist :friends])))
+
+  (def test-game-data (map #(steam/owned-games k %) acrons-friends-ids))
+
+  (def first_example (map :appid (get-in (first foo) [:response :games])))
+
+  (def second_example (map :appid (get-in (second foo) [:response :games])))
+
+  (diff first_example second_example)
+)
