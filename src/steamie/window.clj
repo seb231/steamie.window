00670fed64887c83b43f8ec0b3c90a4e3128e765
Seb Bailey <sebastian@mastodonc.com>
2016-12-09 15:11:52 +0000

    Removed fns for random user generator

--- src/steamie/window.clj
+++ src/steamie/window.clj
@@ -2,24 +2,11 @@
   (:require [steamweb.core :as steam])
   (:gen-class))
 
-(def steam-string-starter "7656119")
-
-(def steam-URL "https://steamcommunity.com/profiles/")
-
 (def config (read-string (slurp (str "config.edn"))))
 
 (def k (:api-key config))
 
 (def acron-id (:id (:acron config)))
-
-(defn rand-4-digit-number [] ;;;; WIP ;;;;
-  (+ (rand-int 1001) 9999))
-
-(defn generate-steam-id-query [starter]
-  (str starter (rand-4-digit-number) (rand-4-digit-number)))
-
-(defn generate-steam-URL []
-  (str steam-URL (generate-steam-id-query steam-string-starter) "/"))
 
 (defn get-friends-list [steam-id]
   (map :steamid (get-in (steam/friend-list k steam-id) [:friendslist :friends])))
