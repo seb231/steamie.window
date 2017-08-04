0ebd0a06aa1048dce22aed2064de4d8590865192
Seb Bailey <sebastian@mastodonc.com>
2017-08-04 14:47:03 +0100

    Fixed tests

--- test/steamie/window_test.clj
+++ test/steamie/window_test.clj
@@ -9,29 +9,34 @@
 
 (def example-database
   "this is example output of collect-database"
-  '({:76561197960375033 {:game_count 10, :games [{:appid 10, :playtime_forever 0} {:appid 20, :playtime_forever 0} {:appid 30, :playtime_forever 0} {:appid 40, :playtime_forever 0} {:appid 50, :playtime_forever 64} {:appid 60, :playtime_forever 0} {:appid 70, :playtime_forever 0} {:appid 130, :playtime_forever 0} {:appid 220, :playtime_forever 430} {:appid 240, :playtime_forever 6}]}} {:76561197960667429 {:game_count 10, :games [{:appid 10, :playtime_forever 13} {:appid 20, :playtime_forever 0} {:appid 30, :playtime_forever 0} {:appid 40, :playtime_forever 0} {:appid 50, :playtime_forever 0} {:appid 60, :playtime_forever 0} {:appid 70, :playtime_forever 1} {:appid 130, :playtime_forever 0} {:appid 80, :playtime_forever 0} {:appid 100, :playtime_forever 0}]}} {:76561197969555013 {:game_count 10, :games [{:appid 10, :playtime_forever 0} {:appid 20, :playtime_forever 0} {:appid 30, :playtime_forever 0} {:appid 40, :playtime_forever 0} {:appid 50, :playtime_forever 64} {:appid 60, :playtime_forever 0} {:appid 70, :playtime_forever 0} {:appid 130, :playtime_forever 0} {:appid 220, :playtime_forever 430} {:appid 240, :playtime_forever 6}]}}))
+  '({:76561197960375033 {:game_count 10, :games [{:appid 10, :playtime_forever 72} {:appid 20, :playtime_forever 0} {:appid 30, :playtime_forever 0} {:appid 40, :playtime_forever 0} {:appid 50, :playtime_forever 64} {:appid 60, :playtime_forever 0} {:appid 70, :playtime_forever 0} {:appid 130, :playtime_forever 0} {:appid 220, :playtime_forever 430} {:appid 240, :playtime_forever 6}]}} {:76561197960667429 {:game_count 10, :games [{:appid 550, :playtime_forever 900} {:appid 20, :playtime_forever 0} {:appid 30, :playtime_forever 0} {:appid 40, :playtime_forever 0} {:appid 50, :playtime_forever 0} {:appid 60, :playtime_forever 0} {:appid 70, :playtime_forever 1} {:appid 130, :playtime_forever 0} {:appid 80, :playtime_forever 0} {:appid 100, :playtime_forever 0}]}} {:76561197969555013 {:game_count 10, :games [{:appid 10, :playtime_forever 0} {:appid 20, :playtime_forever 0} {:appid 30, :playtime_forever 0} {:appid 40, :playtime_forever 0} {:appid 50, :playtime_forever 64} {:appid 60, :playtime_forever 0} {:appid 70, :playtime_forever 0} {:appid 130, :playtime_forever 0} {:appid 220, :playtime_forever 430} {:appid 240, :playtime_forever 6}]}}))
 
 (deftest get-games-out-db-test
   ""
   (= '(10 20 30 40 50 60 70 130 220 240)
      (get-games-out-db (first example-database))))
 
-(deftest search-game-test
+(deftest own-game-test
   ""
-  (is (= true (search-game 10 '(10 20 30)))))
+  (is (= true (own-game? 10 '({:time 72, :appid 10} {:time 0, :appid 20})))))
 
 (deftest match-game-test
   ""
-  (is (= '(:76561197960375033) (match-game 10 (first example-database)))))
+  (is (= {:76561197960375033 {:time 72}} (match-game 10 (first example-database)))))
+
+(deftest match-time-test
+  ""
+  (is (= {:76561197960375033 {:time 50, :appid 10}}
+         (match-time {:appid 10, :playtime_forever 65, :poisson {:upper 80.80202518666516, :lower 49.197974813334845}} {:76561197960375033 {:time 50, :appid 10}}) )))
 
 (deftest users-with-matching-game-test
   ""
-  (is (= [:76561197960375033 :76561197960667429 :76561197969555013]
-         (users-with-matching-game 10 example-database))))
+  (is (= {:76561197960375033 {:time 72}}
+         (users-with-matching-game {:appid 10, :playtime_forever 65, :poisson {:upper 80.80202518666516, :lower 49.197974813334845}} example-database))))
 
 (deftest search-for-matching-games-test
   (let [result (search-for-matching-games example-user-profile example-database)]
     ""
-    (is (= 3 (count result)))
+    (is (= 2 (count result)))
     ""
-    (is (= 3 (count (first (vals (first result))))))))
+    (is (= [{:10 {:76561197960375033 {:time 72}}} {:550 {:76561197960667429 {:time 900}}}] result))))
